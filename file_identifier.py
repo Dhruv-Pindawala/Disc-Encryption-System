f19865784723accb973b1b88948e0004b7b137f6
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2020-12-05 23:55:43 +0530

    encrypted files successfully
    encrypted files but produce a new copy with .aes extension whereas the original file remains as it is. same is the case for decryption

--- file_identifier.py
+++ file_identifier.py
@@ -1,10 +1,28 @@
 import magic
+import os
+import pyAesCrypt
 
 file_input = input("=> Please enter the GLOBAL LOCATION of the file : ")
+file_type = "None"
+
+
 try:
-    file_type_initial = magic.from_file(file_input, mime = True)
-    file_type = file_type_initial[1+file_type_initial.rfind('/'):]
-    print("=> The file format of the given file is {}".format(file_type.upper()))
+    if os.path.isfile(file_input):
+        file_type_initial = magic.from_file(file_input, mime = True)
+        file_type = file_type_initial[1+file_type_initial.rfind('/'):]
+        print("=> The file format of the given file is {}.....".format(file_type.upper()))
+    elif os.path.isdir(file_input):
+        file_type = "directory"
+        print("=> We are dealing with a DIRECTORY.....")
+
 except:
     print("=> Please enter a valid file location.")
-action = input('=> what action do you want to take? Press E to encrypt or D to decrypt')
+
+action = input('=> what action do you want to take? Press E to encrypt or D to decrypt : ').lower()
+password = input("=> Please enter the PASSWORD : ")
+buffersize = 64 * 1024
+
+if action == 'e' and file_type != "None" and file_type != 'directory':
+    pyAesCrypt.encryptFile(file_input, file_input+".aes", password, buffersize)
+elif action == 'd' and file_type != "None" and file_type != 'directory':
+    pyAesCrypt.decryptFile(file_input, file_input[:-4], password, buffersize)
