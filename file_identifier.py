0b2de771acafcd4204652d878c86e4a264a98863
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2020-12-05 20:14:04 +0530

    differentiating files and directtories

--- file_identifier.py
+++ file_identifier.py
@@ -1,8 +1,10 @@
 import magic
 
-file_input = input("Please enter the GLOBAL LOCATION of the file : ")
+file_input = input("=> Please enter the GLOBAL LOCATION of the file : ")
 try:
     file_type_initial = magic.from_file(file_input, mime = True)
     file_type = file_type_initial[1+file_type_initial.rfind('/'):]
+    print("=> The file format of the given file is {}".format(file_type.upper()))
 except:
-    print("Please enter a valid file location.")
+    print("=> Please enter a valid file location.")
+action = input('=> what action do you want to take? Press E to encrypt or D to decrypt')
