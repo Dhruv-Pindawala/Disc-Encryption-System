17ef0f1cf186884a9e01ed1856c80ef6bb0a0d20
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2020-12-05 16:08:54 +0530

    final file type
    gives only the file format and saves it for further use

--- file_identifier.py
+++ file_identifier.py
@@ -2,7 +2,7 @@ import magic
 
 file_input = input("Please enter the GLOBAL LOCATION of the file : ")
 try:
-    file_type = magic.from_file(file_input, mime = True)
-    print(file_type)
+    file_type_initial = magic.from_file(file_input, mime = True)
+    file_type = file_type_initial[1+file_type_initial.rfind('/'):]
 except:
     print("Please enter a valid file location.")
