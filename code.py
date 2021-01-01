09f1de83cae32d57841a4ac29c542e40f4e37779
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-01-01 22:43:00 +0530

    Nea code, able to process files via its global location

---
+++ code.py
@@ -0,0 +1,19 @@
+import os
+import pyAesCrypt
+import getpass
+
+input_file = input("Please provide the global location of the file/folder you want to process : ")
+input_operation = input("=> What action do you want to take? Press E to encrypt or D to decrypt : ").lower()[0]
+input_password = getpass.getpass("Please enter the password you want to use : ")
+buffsize = 64 * 1024
+
+if os.path.isfile(input_file):
+    if input_operation == "e":
+        pyAesCrypt.encryptFile(input_file, input_file + ".aes", input_password, buffsize)
+        os.remove(input_file)
+    elif input_operation == "d":
+        try:
+            pyAesCrypt.decryptFile(input_file, input_file[:-4], input_password, buffsize)
+            os.remove(input_file)
+        except:
+            print("PLEASE ENTER THE CORRECT PASSWORD")
