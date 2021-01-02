a2788eef4a54e61c382fde66d076bef4d1547723
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-01-02 15:47:41 +0530

    able to list global location of folder content

--- code.py
+++ code.py
@@ -10,6 +10,13 @@ print("")
 input_password = getpass.getpass("=> Please enter the password you want to use : ")
 buffsize = 64 * 1024
 
+def folder_action(folder_loc):
+    folder_content = os.listdir(folder_loc)
+    if len(folder_content) > 0:
+        for i in folder_content:
+            present_file = os.path.join(folder_loc, i)
+            print(present_file)
+
 if os.path.isfile(input_file):
     if input_operation == "e":
         pyAesCrypt.encryptFile(input_file, input_file + ".aes", input_password, buffsize)
@@ -20,3 +27,5 @@ if os.path.isfile(input_file):
             os.remove(input_file)
         except:
             print("PLEASE ENTER THE CORRECT PASSWORD")
+
+folder_action(input_file)
